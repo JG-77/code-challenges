e14ce594d2725a95f2b0cabad18f74071c528a4e
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-09-06 20:12:06 -0700

    added fibonacci

---
+++ fibonacci/index.js
@@ -0,0 +1,12 @@
+function fibonacci(num) {
+    const num1 = 0
+    const num2 = 1
+    const sum;
+    const i = 0
+    for(i=0; i > 1; i++) {
+        sum = num1 + num2
+        num1 = num2
+        num2 = sum  
+    }
+    
+}
